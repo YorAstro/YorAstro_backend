c134f0e4fb0ca93e4ec5a74f675ac64ee9187e1d
Chirag Arora <arorachirag858@gmail.com>
2025-03-12 00:30:49 +0530

    category id

--- controllers/product.js
+++ controllers/product.js
@@ -2,7 +2,8 @@ const Product = require('../models/product');
 
 const productList = async (req, res) =>{
     try {
-        const ProductList = await Product.findAll();
+        let categoryId = req.query.categoryId;
+        const ProductList = await Product.findAll({where : {category_id : categoryId}});
         res.status(200).send({data : ProductList});
     } catch (error) {
         console.log(err);
